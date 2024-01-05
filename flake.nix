2c1ebe3ed9826c4c8b1f3835bc8cba009f3e0ba5
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2024-01-05 17:05:24 +0200

    brew-src: 4.1.11 -> 4.1.13

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     flake-utils.url = "github:numtide/flake-utils";
     brew-src = {
-      url = "github:Homebrew/brew/4.2.2";
+      url = "github:Homebrew/brew/4.1.13";
       flake = false;
     };
   };
